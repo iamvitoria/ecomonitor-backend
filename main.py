61bd3ac1d4e4508f8d3de5fa11f5dffeac7baba5
Vitória Camara <vlcamara@inf.ufsm.br>
2026-04-28 09:45:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
     allow_origin_regex=None,
     allow_credentials=True, 
     allow_methods=["*"],
