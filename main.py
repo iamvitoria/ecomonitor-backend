99cfa80fccf5beb4fe0dc2ba8d0cd7234cffbd45
Vitória Camara <vlcamara@inf.ufsm.br>
2026-04-29 09:22:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://ecomonitor-api.onrender.com", 
-        "https://tcc-three-mu.vercel.app/" 
+        "https://tcc-three-mu.vercel.app" 
     ],
     allow_credentials=True, 
     allow_methods=["*"],
