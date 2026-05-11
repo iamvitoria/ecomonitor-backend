3c120b88b64702b38272470dc9e1162821f8dce3
Vitória Camara <vlcamara@inf.ufsm.br>
2026-05-11 11:54:02 -0300

    permitindo qualquer origem do cors

--- main.py
+++ main.py
@@ -17,12 +17,7 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://ecomonitor-api.onrender.com", 
-        "https://tcc-three-mu.vercel.app" 
-    ],
+    allow_origins=["*"],
     allow_credentials=True, 
     allow_methods=["*"],
     allow_headers=["*"],
