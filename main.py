76ae24f105355ad1edc9491d2c1c90cdb738f692
Vitória Camara <vlcamara@inf.ufsm.br>
2026-05-11 16:16:07 -0300

    aceitando origens específicas no cors

--- main.py
+++ main.py
@@ -17,7 +17,12 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://ecomonitor-api.onrender.com", 
+        "https://tcc-three-mu.vercel.app" 
+    ],
     allow_credentials=True, 
     allow_methods=["*"],
     allow_headers=["*"],
